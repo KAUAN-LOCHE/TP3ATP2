76f71500c0c240e0c653c6ca1e0b549dbc9be1d2
KAUAN-LOCHE <127434205+KAUAN-LOCHE@users.noreply.github.com>
2023-11-20 12:08:00 -0300

    Update Funcoes.h
    Adicionei a structs que eu criei, a struct "jogador" e as funções. Confere aí pra ver se não faltou algo!

--- Funcoes.h
+++ Funcoes.h
@@ -5,11 +5,35 @@
 //Variavel string100//
 typedef char string100[100];
 
+//variável CPF 
+typedef char CPF[11];
+
+//Genero
+typedef enum genero{
+    masculino, feminino
+}GENERO;
+
+typedef enum Est_civil{
+    casado, solteiro, divorciado, viúvo
+}EST_CIVIL;
+
+//Data de nascimento
+typedef struct data{
+    int dia;
+    int mes;
+    int ano;
+}DATA;
+
 //Struct Hardware//
 typedef enum computador{
     Desktop, Notebook
 }COMPUTADOR;
 
+typedef struct equipe{
+    string100 nome_equipe;
+    string100 nickname;
+    int seguidores;
+}EQUIPE;
 
 typedef struct hardware{
     COMPUTADOR computer;
@@ -36,6 +60,32 @@ typedef struct ranking{
     int rank;
 }RANKING;
 
+typedef struct jogador{
+    string100 nome;
+    DATA data;
+    CPF cpf;
+    GENERO genero;
+    EST_CIVIL civil;
+    EQUIPE equipe;  
+    string100 patrocinadora;
+    string100 nickname_jogador;
+    int seguidores_jogador;
+    HARDWARE hardware;
+    CAMPEONATO campeonato;
+    TITULOS titulo;
+    RANKING ranking;
+}JOGADOR;
+
+
+typedef struct aux{
+    string100 nome_aux;
+    int ranking_aux;
+    int vitorias_aux;
+    int pontuacao_aux;
+    int derrotas_aux;
+    int empates_aux;
+}AUX;
+
 void EscreverJogadoresVetor(JOGADOR *jogadores, int n);
 void EscreverJogadoresArquivoBinario(JOGADOR *jogadores, int n);
 void LerJogadoresArquivoBinario(JOGADOR *jogadoreslidos, int n);
@@ -44,6 +94,16 @@ void InserirVitoriaEmpateDerrota(JOGADOR *jogadores, int a);
 void BuscarJogadorNome(JOGADOR *jogadores, string100 chave);
 void BuscarJogadorRank(JOGADOR *jogadores, int chave);
 void MostrarMediaSeguidoresJogadores(JOGADOR *jogadores, int n);
+void CriarVetor_aux(JOGADOR* jogadores, AUX * auxiliar, int n);
+void OrdenarJogadores_alfabeto( AUX*auxiliares, int n);
+void OrdenarJogadores_VetorAux(AUX * auxiliar, int n);
+void OrdenarJogadores_posicao( AUX * auxiliar, int n,);
+void OrdenarJogadores_vitoria ( AUX * auxiliar, int n,);
+void ListarJogadores_posicaoCampeonato ( AUX * auxiliar, int n, JOGADOR * jogadores);
+void ListarJogadores_PontuacaoMenor (AUX * auxiliar, int n, int limite);
+void ListarJogadores_PontuacaoMaior (AUX * auxiliar, int n, int limite)
+
+
 
 
 #endif
